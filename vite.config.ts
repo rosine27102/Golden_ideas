e10fc3b9db78c2ab88997e8a9df7be9799e87f1a
rosine27102 <lembarosine3@gmail.com>
2025-09-20 19:59:28 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-     base: '/Golden_ideas/',
+     base: '/Golden_ideas',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
